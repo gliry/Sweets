2da3ebf3a83ae473796da89c6d1933cf8e0f7cd8
gliry <49316522+gliry@users.noreply.github.com>
2021-03-24 01:01:12 +0300

    Made 201 response of 1 POST task

--- lollipops/models.py
+++ lollipops/models.py
@@ -3,12 +3,11 @@ from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth import get_user_model
 from ast import literal_eval
 
-User = get_user_model()
 
 
 class Courier(models.Model):
     courier_id = models.PositiveIntegerField(verbose_name='courier_id', db_index=True, unique=True)
     courier_type = models.CharField(verbose_name='courier_type', max_length=10)
     regions = ArrayField(models.IntegerField(), blank=True, null=True)
-    working_hours = models.CharField(verbose_name='working_hours', max_length=128)
-    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    working_hours = ArrayField(models.CharField(verbose_name='working_hours', max_length=128))
+
--- lollipops/views.py
+++ lollipops/views.py
@@ -1,4 +1,7 @@
 from rest_framework import generics
+from rest_framework.response import Response
+import json
+from django.http import HttpResponse, JsonResponse
 
 from lollipops.models import Courier
 from lollipops.serializers import CourierDetailSerializer, CourierListSerializer
@@ -6,11 +9,30 @@ from lollipops.serializers import CourierDetailSerializer, CourierListSerializer
 
 class CourierCreateView(generics.CreateAPIView):
     serializer_class = CourierDetailSerializer
+    queryset = Courier.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        ser = self.get_serializer(data=request.data["data"], many=True)
+        ser.is_valid(raise_exception=True)
+        ser.save()
+        for i in range(len(ser.data)):
+            del ser.data[i]['courier_type']
+            del ser.data[i]['working_hours']
+            del ser.data[i]['id']
+            del ser.data[i]['regions']
+            ser.data[i]['id'] = ser.data[i].pop('courier_id')
+            values = {
+                "courier": ser.data
+            }
+
+        return Response(values, status=201)
+
 
 
 class CourierListView(generics.ListAPIView):
     serializer_class = CourierListSerializer
     queryset = Courier.objects.all()
+
 
 
 class CourierDetailView(generics.RetrieveDestroyAPIView):
